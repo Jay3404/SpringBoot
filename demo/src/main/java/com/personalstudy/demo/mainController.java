cfbd2f2d1fce40eb5a2372603380460992b6a98f
DESKTOP-439URRO\Jay <the20027@naver.com>
2023-07-03 22:26:01 +0900

    boot start

---
+++ demo/src/main/java/com/personalstudy/demo/mainController.java
@@ -0,0 +1,14 @@
+//package com.personalstudy.demo;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class mainController {
+//    @GetMapping("/")
+//    public String viewMain() {
+//        return "/index";
+//    }
+//
+//
+//}
